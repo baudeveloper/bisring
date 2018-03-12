e7363070e456819aa4ded37efc2f444c61f6a160
Baudeveloper <webmaster@baustein.ca>
2018-03-12 13:09:47 -0400

    Revised template to add Material bootstrap

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,25 +1,25 @@
 //========================
 // Strict Syntax Rendering
 //========================
-"use strict";
+// "use strict";
 
 //==================
 // Application Logic
 //==================
-var myApp = {};
-myApp.init = () => {
-	// const SLANG = "Eh! ";
-	// const SPEAK = "Today, The weather is: ";
-	// let weather = 100;
-	// document.querySelector(".msg").textContent = SLANG + SPEAK + weather;
-	$("a[data-js=\"signup\"]").on("click", function() {
-		alert("You clicked me.");
-	});
-};
+// var myApp = {};
+// myApp.init = () => {
+// 	// const SLANG = "Eh! ";
+// 	// const SPEAK = "Today, The weather is: ";
+// 	// let weather = 100;
+// 	// document.querySelector(".msg").textContent = SLANG + SPEAK + weather;
+// 	$("a[data-js=\"signup\"]").on("click", function() {
+// 		alert("You clicked me.");
+// 	});
+// };
 
 //===============
 // Initialization
 //===============
-$(function() {
-	myApp.init();
-});
+// $(function() {
+// 	myApp.init();
+// });
