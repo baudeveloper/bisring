d8c4d84ddeee57e9d1f65c9247e13e35e6244c3c
Baudeveloper <webmaster@baustein.ca>
2018-03-13 12:09:32 -0400

    Added sidenav and related scripts

--- app/js/custom.js
+++ app/js/custom.js
@@ -6,8 +6,24 @@
 //==================
 // Application Logic
 //==================
-$(document).ready(function() {
+$(document).ready(function () {
+
 	var siteCallout = $("[data-js=\"site-callout\"]");
 	var imgSrc = siteCallout.find(".site-callout--img").attr("src");
-	siteCallout.css("backgroundImage", "url("+imgSrc+")");
+	siteCallout.css("backgroundImage", "url(" + imgSrc + ")");
+
+	// Source: https://bootstrapious.com/tutorial/sidebar/index3.html
+	$("#sidebar").mCustomScrollbar({
+		theme: "minimal"
+	});
+	$("#sidebarCollapse").on("click", function () {
+		$("#sidebar").addClass("active");
+		$(".overlay").fadeIn();
+		$(".collapse.in").toggleClass("in");
+		$("a[aria-expanded=true]").attr("aria-expanded", "false");
+	});
+	$("#dismiss, .overlay").on("click", function () {
+		$("#sidebar").removeClass("active");
+		$(".overlay").fadeOut();
+	});
 });
