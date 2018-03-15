f2c1ec973637b7782580bb3c51e11d55fab93e3f
Baudeveloper <webmaster@baustein.ca>
2018-03-15 09:00:25 -0400

    Revised site structure

--- app/js/custom.js
+++ app/js/custom.js
@@ -8,6 +8,7 @@
 //==================
 $(document).ready(function () {
 
+	// Banner backgrounds
 	var siteCallout = $("[data-js=\"site-callout\"]");
 	var imgSrc = siteCallout.find(".site-callout--img").attr("src");
 	siteCallout.css("backgroundImage", "url(" + imgSrc + ")");
@@ -15,29 +16,31 @@ $(document).ready(function () {
 	var imgHeaderSrc = siteHeader.find(".site-header--img").attr("src");
 	siteHeader.css("backgroundImage", "url(" + imgHeaderSrc + ")");
 
+	// Sidebar
 	// Source: https://bootstrapious.com/tutorial/sidebar/index3.html
-	var offCanvasMenu = $("[data-js=\"offcanvas\"]");
-	var offCanvasCollapse = $("[data-js=\"offcanvas-collapse\"]");
-	var offCanvasDismiss = $("[data-js=\"offcanvas-dismiss\"]");
-	var offCanvasOverlay = $("[data-js=\"offcanvas-overlay\"]");
-	offCanvasMenu.mCustomScrollbar({
+	var sidenavMenu = $("[data-js=\"sidenav\"]");
+	var sidenavCollapse = $("[data-js=\"sidenav-collapse\"]");
+	var sidenavDismiss = $("[data-js=\"sidenav-dismiss\"]");
+	var sidenavOverlay = $("[data-js=\"sidenav-overlay\"]");
+	sidenavMenu.mCustomScrollbar({
 		theme: "minimal"
 	});
-	offCanvasCollapse.on("click", function () {
-		offCanvasMenu.addClass("active");
-		offCanvasOverlay.fadeIn();
+	sidenavCollapse.on("click", function () {
+		sidenavMenu.addClass("active");
+		sidenavOverlay.fadeIn();
 		$(".collapse.in").toggleClass("in");
 		$("a[aria-expanded=true]").attr("aria-expanded", "false");
 	});
-	offCanvasDismiss.on("click", function () {
-		offCanvasMenu.removeClass("active");
-		offCanvasOverlay.fadeOut();
+	sidenavDismiss.on("click", function () {
+		sidenavMenu.removeClass("active");
+		sidenavOverlay.fadeOut();
 	});
-	offCanvasOverlay.on("click", function () {
-		offCanvasMenu.removeClass("active");
-		offCanvasOverlay.fadeOut();
+	sidenavOverlay.on("click", function () {
+		sidenavMenu.removeClass("active");
+		sidenavOverlay.fadeOut();
 	});
 
+	// Popovers
 	// Source: https://stackoverflow.com/questions/24580262/make-bootstrap-popover-work-with-custom-html-template
 	var popoverTemplate = [
 		"<div class=\"popover\" role=\"tooltip\">",
@@ -73,6 +76,7 @@ $(document).ready(function () {
 		html: true
 	});
 
+	// Selectpickers
 	$(".selectpicker").selectpicker();
 
 });
